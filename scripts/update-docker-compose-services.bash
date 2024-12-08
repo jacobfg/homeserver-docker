26ce4e7d8a62ce8c1bd632b23a8911ccf971301c
Jacob Fleming-Gale <10949267+jacobfg@users.noreply.github.com>
2024-12-08 18:13:49 +1100

    feat: refactor dir

--- scripts/update-docker-compose-services.bash
+++ scripts/update-docker-compose-services.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
+SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)/.."
 
 # update each stack
 while read -d $'\0' STACK ; do
