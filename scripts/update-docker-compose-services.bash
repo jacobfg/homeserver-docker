bb98a56eab8c562e1055d2698d133d5ceb14ff35
Jacob Fleming-Gale <10949267+jacobfg@users.noreply.github.com>
2024-12-08 19:04:10 +1100

    feat: refactor dir

--- scripts/update-docker-compose-services.bash
+++ scripts/update-docker-compose-services.bash
@@ -16,21 +16,22 @@ while read -d $'\0' STACK ; do
     echo $STACK
 
     # pull images then update and remove orphans
-    # docker compose pull
-    # docker compose up -d --remove-orphans
+    docker compose pull
+    docker compose up -d --remove-orphans
 
     # workflow uses digest, tag local image for niceness
-    # yq -r '.services[].image' docker-compose.yml | grep '@sha256:' | sed 's~^\([^:]\+\):\([^@]\+\)@\(.\+\)$~\1@\3 \1:\2~' | xargs -n2 docker tag
+    yq -r '.services[].image' docker-compose.yml | grep '@sha256:' | sed 's~^\([^:]\+\):\([^@]\+\)@\(.\+\)$~\1@\3 \1:\2~' | xargs -n2 docker tag
+
 done < <(findOrderStacks)
 
-# # clean all unused images
-# docker image prune -f
+# clean all unused images
+docker image prune -f
 
-# # prune everything
-# # docker system prune -af
+# prune everything
+# docker system prune -af
 
-# # open ports
-# # ss -tulpen | grep -vEe "\s+127[.]|::1|239[.]255[.]255[.]250|fd4e:ca90:4df3:3f48:e65f:1ff:fe45:5c78|fe80::e65f:1ff:fe45:5c78"
+# open ports
+# ss -tulpen | grep -vEe "\s+127[.]|::1|239[.]255[.]255[.]250|fd4e:ca90:4df3:3f48:e65f:1ff:fe45:5c78|fe80::e65f:1ff:fe45:5c78"
 
-# # ports open to LAN (IPv4)
-# ss -tnlp4 '! src 127.0.0.0/8'
+# ports open to LAN (IPv4)
+ss -tnlp4 '! src 127.0.0.0/8'
