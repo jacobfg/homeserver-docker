ecfa3dd452a27b489eeffa20573c986550305d68
Jacob Fleming-Gale <10949267+jacobfg@users.noreply.github.com>
2024-12-12 22:04:32 +1100

    feat: use git-secret

--- scripts/update-docker-compose-services.bash
+++ scripts/update-docker-compose-services.bash
@@ -15,7 +15,7 @@ while read -d $'\0' STACK ; do
     cd "$SCRIPT_DIR/../$STACK"
     echo $STACK
 
-    export $SECRETS_DIR=$SCRIPT_DIR/../secrets
+    export SECRETS_DIR=$SCRIPT_DIR/../secrets
 
     # pull images then update and remove orphans
     docker compose pull
